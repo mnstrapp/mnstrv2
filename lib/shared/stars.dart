598736532ff0c2f381e076127debac10a200f495
Michael Christenson II <m3talsmith@gmail.com>
2025-07-13 06:50:55 -0400

    Shimmering stars

--- lib/shared/stars.dart
+++ lib/shared/stars.dart
@@ -12,6 +12,8 @@ class _StarsViewState extends State<StarsView> with TickerProviderStateMixin {
   final List<AnimationController> _controllers = [];
   final List<Animation<Offset>> _animations = [];
   final List<Offset> _targetPositions = [];
+  final int _starsCount = 1000;
+  final List<Widget> _stars = [];
 
   @override
   void initState() {
@@ -20,18 +22,13 @@ class _StarsViewState extends State<StarsView> with TickerProviderStateMixin {
   }
 
   void _setupAnimations() {
-    for (var i = 0; i < 300; i++) {
+    for (var i = 0; i < _starsCount; i++) {
       final controller = AnimationController(
         duration: Duration(milliseconds: 1000 + Random().nextInt(1000)),
         vsync: this,
         reverseDuration: Duration(milliseconds: 1000 + Random().nextInt(1000)),
       );
-      controller.addStatusListener((status) async {
-        if (status == AnimationStatus.completed) {
-          await Future.delayed(Duration(milliseconds: 3000));
-          controller.reverse();
-        }
-      });
+      controller.addStatusListener(_checkStatus(i));
 
       final targetPosition = _generateRandomTarget();
       _targetPositions.add(targetPosition);
@@ -51,7 +48,6 @@ class _StarsViewState extends State<StarsView> with TickerProviderStateMixin {
       _controllers.add(controller);
       _animations.add(animation);
 
-      // Start animation with staggered delay
       Future.delayed(Duration(milliseconds: Random().nextInt(500)), () {
         if (mounted) controller.forward();
       });
@@ -59,38 +55,26 @@ class _StarsViewState extends State<StarsView> with TickerProviderStateMixin {
   }
 
   void Function(AnimationStatus) _checkStatus(int index) {
-    return (status) async {
+    return (status) {
       if (status == AnimationStatus.completed) {
-        await Future.delayed(Duration(milliseconds: 3000));
-        _controllers[index].reverse();
+        Future.delayed(
+          Duration(milliseconds: 3000 + Random().nextInt(1000)),
+          () => _controllers[index].reverse(),
+        );
       }
     };
   }
 
   Offset _generateRandomTarget() {
     final random = Random();
-    // Generate position near edge (0.1 to 0.9 range)
-    final x = random.nextDouble() * 0.8 + 0.1;
-    final y = random.nextDouble() * 0.8 + 0.1;
+    final x = random.nextDouble();
+    final y = random.nextDouble();
     return Offset(x, y);
   }
 
-  @override
-  void dispose() {
-    for (final controller in _controllers) {
-      controller.removeStatusListener((status) {});
-      controller.dispose();
-    }
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final List<Widget> stars = [];
-
-    for (var i = 0; i < 300; i++) {
-      stars.add(
+  void _addStars(Size size) {
+    for (var i = 0; i < _starsCount; i++) {
+      _stars.add(
         AnimatedBuilder(
           animation: _animations[i],
           builder: (context, child) {
@@ -103,21 +87,96 @@ class _StarsViewState extends State<StarsView> with TickerProviderStateMixin {
         ),
       );
     }
+  }
 
-    return Stack(children: stars);
+  @override
+  void dispose() {
+    for (var i = 0; i < _starsCount; i++) {
+      _controllers[i].removeStatusListener(_checkStatus(i));
+      _controllers[i].dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    _addStars(size);
+
+    return Stack(children: _stars);
   }
 }
 
-class _StarView extends StatelessWidget {
+class _StarView extends StatefulWidget {
   const _StarView();
+
+  @override
+  State<_StarView> createState() => _StarViewState();
+}
+
+class _StarViewState extends State<_StarView> with TickerProviderStateMixin {
+  AnimationController? _controller;
+  Animation<double>? _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _setupAnimation();
+  }
+
+  void _setupAnimation() {
+    final duration = Duration(milliseconds: 100 + Random().nextInt(400));
+    _controller = AnimationController(
+      duration: duration,
+      vsync: this,
+      reverseDuration: duration,
+    );
+
+    _controller?.addStatusListener(_checkStatus);
+
+    _opacityAnimation = Tween<double>(
+      begin: 1,
+      end: 0.7,
+    ).animate(CurvedAnimation(parent: _controller!, curve: Curves.easeOutExpo));
+
+    _controller?.forward();
+  }
+
+  void _checkStatus(AnimationStatus status) {
+    if (status == AnimationStatus.completed) {
+      Future.delayed(Duration(milliseconds: 100 + Random().nextInt(400)), () {
+        _controller?.reverse();
+      });
+    }
+    if (status == AnimationStatus.dismissed) {
+      Future.delayed(Duration(milliseconds: 100 + Random().nextInt(400)), () {
+        _controller?.forward();
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.removeStatusListener(_checkStatus);
+    _controller?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final starIndex = Random().nextInt(5) + 1;
-    return Image.asset(
-      'assets/stars/star_$starIndex.png',
-      width: 25,
-      height: 25,
+    return AnimatedBuilder(
+      animation: _opacityAnimation!,
+      builder: (context, child) {
+        return Opacity(
+          opacity: _opacityAnimation!.value,
+          child: Image.asset(
+            'assets/stars/star_$starIndex.png',
+            width: 25,
+            height: 25,
+          ),
+        );
+      },
     );
   }
 }
