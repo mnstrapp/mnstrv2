fc756fd9b434717cda3330219182c77a473b2344
Michael Christenson II <christensonmi.ctr@socom.mil>
2025-07-15 06:48:42 -0400

    Add background music

--- lib/app.dart
+++ lib/app.dart
@@ -2,16 +2,42 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:just_audio/just_audio.dart';
+
 import 'theme.dart';
 import 'providers/auth.dart';
 import 'home/home.dart';
 import 'auth/login.dart';
 
-class App extends ConsumerWidget {
+class App extends ConsumerStatefulWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<App> createState() => _AppState();
+}
+
+class _AppState extends ConsumerState<App> {
+  late final _audioPlayer = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer.setAudioSource(
+      AudioSource.asset('assets/sound/mnstr-game-music.m4a'),
+    );
+    _audioPlayer.setLoopMode(LoopMode.all);
+    _audioPlayer.play();
+    _audioPlayer.setVolume(1);
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final auth = ref.watch(authProvider);
     return MaterialApp(
       home: auth.when(
