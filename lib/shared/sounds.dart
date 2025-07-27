9bcb737791c0d2e48945645d05ab56369541eb02
Michael Christenson II <m3talsmith@gmail.com>
2025-07-27 11:58:11 -0400

    Loop background music

--- lib/shared/sounds.dart
+++ lib/shared/sounds.dart
@@ -5,17 +5,30 @@ import 'package:flame_audio/flame_audio.dart';
 class BackgroundMusic {
   static const String _backgroundMusic = 'mnstr-game-music.m4a';
 
+  Future<void> loop() async {
+    Timer.periodic(const Duration(microseconds: 100), (timer) async {
+      if (!FlameAudio.bgm.isPlaying) {
+        timer.cancel();
+        FlameAudio.bgm.play(_backgroundMusic);
+        loop();
+      }
+    });
+  }
+
   Future<void> play() async {
     FlameAudio.bgm.initialize();
     FlameAudio.bgm.play(_backgroundMusic);
+    loop();
   }
 
   Future<void> pause() async {
     FlameAudio.bgm.pause();
+    loop();
   }
 
   Future<void> resume() async {
     FlameAudio.bgm.resume();
+    loop();
   }
 }
 
