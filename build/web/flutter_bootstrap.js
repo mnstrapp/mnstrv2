22b7ddd062b35861c7ec52b22c018eb353a5405f
Michael Christenson II <m3talsmith@gmail.com>
2025-10-08 17:16:58 -0400

    Updated messaging

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3720775340"
+    serviceWorkerVersion: "2864217557"
   }
 });
