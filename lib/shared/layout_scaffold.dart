d6cffecd43faea472a16f76209ef5f3f9ee70b7c
Michael Christenson II <m3talsmith@gmail.com>
2025-10-08 14:13:50 -0400

    Make messages great again?

--- lib/shared/layout_scaffold.dart
+++ lib/shared/layout_scaffold.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../theme.dart';
 import 'monster_xp_bar.dart';
 
 class LayoutScaffold extends ConsumerStatefulWidget {
@@ -63,6 +64,27 @@ class LayoutScaffold extends ConsumerStatefulWidget {
   ) {
     of(context).setDisableBackButton(disableBackButton);
   }
+
+  static void addMessage(
+    BuildContext context,
+    String message,
+    Color backgroundColor,
+    Color foregroundColor,
+  ) {
+    of(context).addMessage(message, backgroundColor, foregroundColor);
+  }
+
+  static void addError(BuildContext context, String message) {
+    of(context).addError(message);
+  }
+
+  static void addSuccess(BuildContext context, String message) {
+    of(context).addSuccess(message);
+  }
+
+  static void addInfo(BuildContext context, String message) {
+    of(context).addInfo(message);
+  }
 }
 
 class LayoutScaffoldState extends ConsumerState<LayoutScaffold> {
@@ -71,6 +93,7 @@ class LayoutScaffoldState extends ConsumerState<LayoutScaffold> {
   bool _useSafeArea = false;
   bool _showStatBar = true;
   bool _disableBackButton = false;
+  final List<_Message> _messages = [];
 
   @override
   void initState() {
@@ -116,6 +139,35 @@ class LayoutScaffoldState extends ConsumerState<LayoutScaffold> {
     });
   }
 
+  void addMessage(
+    String message,
+    Color backgroundColor,
+    Color foregroundColor,
+  ) {
+    setState(() {
+      _messages.add(
+        _Message(
+          message: message,
+          backgroundColor: backgroundColor,
+          foregroundColor: foregroundColor,
+          onRemove: () {},
+        ),
+      );
+    });
+  }
+
+  void addError(String message) {
+    addMessage(message, Colors.red, Colors.white);
+  }
+
+  void addSuccess(String message) {
+    addMessage(message, Colors.green, Colors.white);
+  }
+
+  void addInfo(String message) {
+    addMessage(message, primaryColor, Colors.white);
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -150,7 +202,79 @@ class LayoutScaffoldState extends ConsumerState<LayoutScaffold> {
                 ),
               ),
             ),
+          if (_messages.isNotEmpty)
+            Positioned(
+              top: 16,
+              left: 0,
+              right: 0,
+              child: Column(
+                spacing: 16,
+                children: _messages
+                    .map(
+                      (message) => message.copyWith(
+                        onRemove: () {
+                          setState(() {
+                            _messages.remove(message);
+                          });
+                        },
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
         ],
+      ),
+    );
+  }
+}
+
+class _Message extends StatelessWidget {
+  final String message;
+  final Color backgroundColor;
+  final Color foregroundColor;
+  final VoidCallback onRemove;
+
+  const _Message({
+    required this.message,
+    required this.backgroundColor,
+    required this.foregroundColor,
+    required this.onRemove,
+  });
+
+  _Message copyWith({
+    String? message,
+    Color? backgroundColor,
+    Color? foregroundColor,
+    VoidCallback? onRemove,
+  }) {
+    return _Message(
+      message: message ?? this.message,
+      backgroundColor: backgroundColor ?? this.backgroundColor,
+      foregroundColor: foregroundColor ?? this.foregroundColor,
+      onRemove: onRemove ?? this.onRemove,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final size = MediaQuery.sizeOf(context);
+
+    return Container(
+      width: size.width * 0.66,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: backgroundColor.withValues(alpha: 0.75),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: InkWell(
+        onTap: onRemove,
+        child: Text(
+          message,
+          style: theme.textTheme.bodyMedium?.copyWith(
+            color: foregroundColor,
+          ),
+        ),
       ),
     );
   }
