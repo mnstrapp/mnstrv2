9cb524c0a038ce345d31be4b6cff4e033cc12d52
Michael Christenson II <m3talsmith@gmail.com>
2026-05-16 15:21:27 -0400

    Fixed mnstr head issue

--- lib/shared/monster_model.dart
+++ lib/shared/monster_model.dart
@@ -2,9 +2,9 @@ import 'dart:convert';
 
 import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 import '../models/monster.dart';
-import '../main.dart';
 
 class MonsterModel {
   String? id;
@@ -116,19 +116,24 @@ class MonsterParts {
     final factor = scale / 2;
     final width = size.width * factor;
     final height = size.height * factor;
-    final headImage = monster.head == 0
-        ? (backside
-              ? AssetImage('assets/mnstr_parts/head_1-back.png')
-              : AssetImage('assets/mnstr_parts/head_1.png'))
-        : (backside
-              ? AssetImage('assets/mnstr_parts/head_2-back.png')
-              : AssetImage('assets/mnstr_parts/head_2.png'));
-    return Image(
-      key: UniqueKey(),
-      image: headImage,
+    if (monster.head == 0) {
+      return Image.asset(
+        backside
+            ? 'assets/mnstr_parts/head_1-back.png'
+            : 'assets/mnstr_parts/head_1.png',
+        width: width,
+        height: height,
+        color: monster.color?.withValues(alpha: 0.75),
+        colorBlendMode: BlendMode.srcATop,
+      );
+    }
+    return Image.asset(
+      backside
+          ? 'assets/mnstr_parts/head_2-back.png'
+          : 'assets/mnstr_parts/head_2.png',
       width: width,
       height: height,
-      color: monster.color,
+      color: monster.color?.withValues(alpha: 0.75),
       colorBlendMode: BlendMode.srcATop,
     );
   }
@@ -137,35 +142,53 @@ class MonsterParts {
     final factor = scale / 2;
     final width = size.width * factor;
     final height = size.height * factor;
-    final hornsImage = switch (monster.horns) {
-      0 => AssetImage('assets/mnstr_parts/horns_short.png'),
-      1 => AssetImage('assets/mnstr_parts/horns_spiraled.png'),
-      2 => AssetImage('assets/mnstr_parts/horns_striped.png'),
-      _ => AssetImage('assets/mnstr_parts/horns_short.png'),
-    };
-    return Image(
-      key: UniqueKey(),
-      image: hornsImage,
-      width: width,
-      height: height,
-      color: monster.color,
-      colorBlendMode: BlendMode.srcATop,
-    );
+    switch (monster.horns) {
+      case 0:
+        return Image.asset(
+          'assets/mnstr_parts/horns_short.png',
+          width: width,
+          height: height,
+          color: monster.color?.withValues(alpha: 0.75),
+          colorBlendMode: BlendMode.srcATop,
+        );
+      case 1:
+        return Image.asset(
+          'assets/mnstr_parts/horns_spiraled.png',
+          width: width,
+          height: height,
+          color: monster.color?.withValues(alpha: 0.75),
+          colorBlendMode: BlendMode.srcATop,
+        );
+      case 2:
+        return Image.asset(
+          'assets/mnstr_parts/horns_striped.png',
+          width: width,
+          height: height,
+          color: monster.color?.withValues(alpha: 0.75),
+          colorBlendMode: BlendMode.srcATop,
+        );
+    }
+    return null;
   }
 
   Widget get arms {
     final factor = scale / 2;
     final width = size.width * factor;
     final height = size.height * factor;
-    final armsImage = monster.arms == 0
-        ? AssetImage('assets/mnstr_parts/arms_two.png')
-        : AssetImage('assets/mnstr_parts/arms_four.png');
-    return Image(
-      key: UniqueKey(),
-      image: armsImage,
+    if (monster.arms == 0) {
+      return Image.asset(
+        'assets/mnstr_parts/arms_two.png',
+        width: width,
+        height: height,
+        color: monster.color?.withValues(alpha: 0.75),
+        colorBlendMode: BlendMode.srcATop,
+      );
+    }
+    return Image.asset(
+      'assets/mnstr_parts/arms_four.png',
       width: width,
       height: height,
-      color: monster.color,
+      color: monster.color?.withValues(alpha: 0.75),
       colorBlendMode: BlendMode.srcATop,
     );
   }
@@ -174,15 +197,20 @@ class MonsterParts {
     final factor = scale / 2;
     final width = size.width * factor;
     final height = size.height * factor;
-    final legsImage = monster.legs == 0
-        ? AssetImage('assets/mnstr_parts/legs_long.png')
-        : AssetImage('assets/mnstr_parts/legs_short.png');
-    return Image(
-      key: UniqueKey(),
-      image: legsImage,
+    if (monster.legs == 0) {
+      return Image.asset(
+        'assets/mnstr_parts/legs_long.png',
+        width: width,
+        height: height,
+        color: monster.color?.withValues(alpha: 0.75),
+        colorBlendMode: BlendMode.srcATop,
+      );
+    }
+    return Image.asset(
+      'assets/mnstr_parts/legs_short.png',
       width: width,
       height: height,
-      color: monster.color,
+      color: monster.color?.withValues(alpha: 0.75),
       colorBlendMode: BlendMode.srcATop,
     );
   }
@@ -191,32 +219,44 @@ class MonsterParts {
     final factor = scale / 2;
     final width = size.width * factor;
     final height = size.height * factor;
-    final tailImage = monster.tail == 0
-        ? AssetImage('assets/mnstr_parts/tail_long.png')
-        : monster.tail == 1
-        ? AssetImage('assets/mnstr_parts/tail_twins.png')
-        : AssetImage('assets/mnstr_parts/tail_stripes.png');
-    return Image(
-      key: UniqueKey(),
-      image: tailImage,
-      width: width,
-      height: height,
-      color: monster.color,
-      colorBlendMode: BlendMode.srcATop,
-    );
+    switch (monster.horns) {
+      case 0:
+        return Image.asset(
+          'assets/mnstr_parts/tail_long.png',
+          width: width,
+          height: height,
+          color: monster.color?.withValues(alpha: 0.75),
+          colorBlendMode: BlendMode.srcATop,
+        );
+      case 1:
+        return Image.asset(
+          'assets/mnstr_parts/tail_twins.png',
+          width: width,
+          height: height,
+          color: monster.color?.withValues(alpha: 0.75),
+          colorBlendMode: BlendMode.srcATop,
+        );
+      case 2:
+        return Image.asset(
+          'assets/mnstr_parts/tail_stripes.png',
+          width: width,
+          height: height,
+          color: monster.color?.withValues(alpha: 0.75),
+          colorBlendMode: BlendMode.srcATop,
+        );
+    }
+    return null;
   }
 
-  Widget get body {
+  Image get body {
     final factor = scale / 2;
     final width = size.width * factor;
     final height = size.height * factor;
-    final bodyImage = AssetImage('assets/mnstr_parts/body_base.png');
-    return Image(
-      key: UniqueKey(),
-      image: bodyImage,
+    return Image.asset(
+      'assets/mnstr_parts/body_base.png',
       width: width,
       height: height,
-      color: monster.color,
+      color: monster.color?.withValues(alpha: 0.75),
       colorBlendMode: BlendMode.srcATop,
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import audio_session
 import audioplayers_darwin
 import just_audio
 import mobile_scanner
+import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import sqflite_darwin
@@ -18,6 +19,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
